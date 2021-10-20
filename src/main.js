08badd1c89cc95c8d3a87734575e01b178db1348
Konstantin Zavgorodniy <konstantin.zavgorodniy@jci.com>
2021-10-20 15:04:12 -0400

    add conversion to bool from string

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ const github = require("@actions/github")
 
 async function main() {
     const regex_patterns = core.getInput("title-regex").split(";")
-    const raise_failure = core.getInput("fail-action-on-regex-failure")
-    const request_changes = core.getInput("request-changes-on-regex-failure")
+    const raise_failure = (core.getInput("fail-action-on-regex-failure").toLowerCase() === "true")
+    const request_changes = (core.getInput("request-changes-on-regex-failure").toLowerCase() === "true")
     const failure_comment = core.getInput("comment-on-regex-failure")
 
     // TODO Remove, this is for debug prints
