a7a514252718b8e89a3b4b92dc693c3c9775ab12
Konstantin Zavgorodniy <konstantin.zavgorodniy@jci.com>
2021-10-20 16:50:30 -0400

    debug prints

--- src/main.js
+++ src/main.js
@@ -3,20 +3,14 @@ const github = require("@actions/github")
 
 async function main() {
     const regex_patterns = core.getInput("title-regex").split(";")
-    const raise_failure = (core.getInput("fail-action-on-regex-failure").toLowerCase() === "true")
-    const request_changes = (core.getInput("request-changes-on-regex-failure").toLowerCase() === "true")
+    const raise_failure = core.getBooleanInput("fail-action-on-regex-failure")
+    const request_changes = core.getBooleanInput("request-changes-on-regex-failure")
     const failure_comment = core.getInput("comment-on-regex-failure")
 
-    // TODO Remove, this is for debug prints
-    core.info(typeOf(regex_patterns))
-    core.info(typeOf(raise_failure))
-    core.info(typeOf(request_changes))
-    core.info(typeOf(failure_comment))
-
-
+    
     try {
-        core.error("core.error PLACEHOLDER!")
-        throw new Error("throw PLACEHOLDER!")
+        logMinimizer(github)
+        logMinimizer(github.context)
     } catch (error) {
         logMinimizer("Event context data",github.context)
         core.setFailed(`Action failed. ${error}`)
