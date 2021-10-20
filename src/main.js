48eb560f201164531ec3b5424a4e910e42b01669
Konstantin Zavgorodniy <konstantin.zavgorodniy@jci.com>
2021-10-20 14:43:33 -0400

    boilerplate

--- src/main.js
+++ src/main.js
@@ -1,6 +1,32 @@
 const core = require("@actions/core")
 const github = require("@actions/github")
 
-async function main() {}
+async function main() {
+    const regex_patterns = core.getInput("title-regex").split(";")
+    const raise_failure = core.getInput("fail-action-on-regex-failure")
+    const request_changes = core.getInput("request-changes-on-regex-failure")
+    const failure_comment = core.getInput("comment-on-regex-failure")
+
+    try {
+        core.error("core.error PLACEHOLDER!")
+        throw new Error("throw PLACEHOLDER!")
+    } catch (error) {
+        logMinimizer("Event context data",github.context)
+        core.setFailed(`Action failed. ${error}`)
+    }
+}
+
+
+function logMinimizer(title, text_to_print) {
+    // prints into a log with ability to maximize/minimize entry
+    core.startGroup(title)
+    console.log(text_to_print)
+    core.endGroup()
+}
+
+function logSeparator() {
+    core.info("=".repeat(80))
+}
+
 
 main()
