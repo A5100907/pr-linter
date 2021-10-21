4f9ee4fdfcefcb0cc423402513d9d3a4f6c28e4a
Konstantin Zavgorodniy <80964867+jzavgok@users.noreply.github.com>
2021-10-21 13:05:56 -0400

    [SDO-2516] Implement pr-linter v1.0 (#1)
    - Supports a title check against specified regex

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-# pr-linter
+# Pull Request Linter
+
+Validates a syntax of the pull request title against specified regexes.
+
+## Using the action
+
+### Inputs
+* __github_token__ - GitHub authentication token, can be passed in as '${{ secrets.GITHUB_TOKEN }}'.
+    * required: true
+
+* __title_regex__ - Semicolon separated list of Regex patterns to validate the pull request title against
+    * required: true
+    * NOTE: Do not use " or ' characters to wrap the string, pass as is (see example)
+
+### Usage
+#### Example:
+```
+name: "Pull Request Linter"
+ 
+on:
+  pull_request:
+    types: ['opened', 'edited', 'reopened', 'synchronize']
+ 
+jobs:
+  execute:
+    name: pr-linter
+    runs-on: ubuntu-latest
+    steps:
+      - name: run-pr-linter
+        uses: A5100907/pr-linter@feature/SDO-2516
+        with:
+          github_token: ${{ secrets.GITHUB_TOKEN }}
+          title_regex: ^(\[([A-Z0-9]{1,6}-[0-9]{1,6})\] [a-zA-Z0-9 \!\?\'\"\=\.\,\(\)\_\-\+]*)$;^(\[(BF|HF)\] [a-zA-Z0-9 \!\?\'\"\=\.\,\(\)\_\-\+]*)$
+               
+```
+#### This would enforce following:
+
+  * Valid:
+  ```
+  [M-1] valid Example of a feature PR title 1234=-+_.,!?'"()
+  [IQ4-101] Yet Another valid Example of a PR of a feature
+  
+  [BF] Valid BugFix title
+  [HF] Valid HotFix title 1234=-+_.,!?'"()
+  ```
+
+  * Invalid:
+  ```
+  [IQ4-101]Invalid PR title because there is no space between a ticket and the beginning of the description
+  [I4-1]:Invalid because ':' is not a valid character
+  [IA-11] Title @#$%^&*{};\/~:` these are the characters that are not allowed
+  ```
---
+++ src/main.js
@@ -0,0 +1,76 @@
+const core = require("@actions/core")
+const github = require("@actions/github")
+
+async function main() {
+    const regex_patterns = core.getInput("title_regex").split(";")
+    const pr_title = github.context.payload.pull_request.title
+    
+    try {
+        logSeparator()
+        logMinimizer("github.context", github.context)
+        logMinimizer("github.context.payload.pull_request.title", github.context.payload.pull_request.title)
+        logSeparator()
+
+        if (isPrTitleValid(regex_patterns, pr_title)) {
+            logSeparator()
+            core.info("Exiting gracefully ...")
+            return
+        } else {
+            logSeparator()
+            core.error("PR Title did not pass regex validation.")
+            throw new Error("Pull Request did not pass naming rules policy!")
+        }
+
+
+    } catch (error) {
+        core.setFailed(`Action failed. ${error}`)
+    }
+}
+
+
+function logMinimizer(title, text_to_print) {
+    // prints into a log with ability to maximize/minimize entry
+    core.startGroup(title)
+    console.log(text_to_print)
+    core.endGroup()
+}
+
+function logSeparator() {
+    core.info("=".repeat(80))
+}
+
+function isPrTitleValid(regexes, pr_title) {
+    // Validates a PR title against a list of regex patterns
+    // Need to match at least one pattern for it to be valid
+    // Returns false if not valid, true if valid
+    core.debug("Running 'isPrTitleValid()' ...")
+    let pr_title_is_valid = false
+    try {
+        regexes.forEach(pattern => {
+            core.info(`Validating '${pr_title}' against '${pattern}'`)
+            const regex = RegExp(pattern)
+            if (regex.test(pr_title)) {
+                core.info("Match.")
+                pr_title_is_valid = true
+            } else {
+                core.info("Not a match.")
+            }
+        })
+
+        if (pr_title_is_valid) {
+            core.info("PR Title matched at least one pattern.")
+            core.info("PR Title is valid.")
+            return true
+        }
+        core.warning("PR Title did not match against any of the allowed regex patterns.")
+        core.warning("Please refer to the PR Title naming policy for your project.")
+        return false
+    } catch (error) {
+        core.error(error)
+        core.error("Something went wrong in regex pattern validation!")
+        core.error("Contact Fusion DevOps team <fusion_devops@johnsoncontrols365.onmicrosoft.com>")
+        throw new Error("isPrTitleValid() failed.")
+    }
+}
+
+main()
