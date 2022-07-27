10b23549bd9d31db7d7fcf834fc615996282cea3
Konstantin Zavgorodniy <konstantin.zavgorodniy@jci.com>
2022-07-27 10:30:13 -0400

    getting label value

--- src/main.js
+++ src/main.js
@@ -1,39 +1,50 @@
+// TODO cleanup form debug prints
+// import necessary modules
 const core = require("@actions/core")
 const github = require("@actions/github")
 
 async function main() {
+    // main
+
+    // set constant values
     const regex_patterns = core.getInput("title_regex").split(";")
     const enable_labeler = (core.getInput("enable_labeler") === "true")
     const pr_title = github.context.payload.pull_request.title
     
     try {
+        // print out available data from github
         logSeparator()
         logMinimizer("github.context", github.context)
         logMinimizer("github.context.payload.pull_request.title", github.context.payload.pull_request.title)
         logSeparator()
 
+        // validate PR and throw an error if it fails
         if (!isPrTitleValid(regex_patterns, pr_title)) {
             logSeparator()
             core.error("PR Title did not pass regex validation.")
             throw new Error("Pull Request did not pass naming rules policy!")
         }
         logSeparator()
-        if (enable_labeler) {
-            const pr_head = github.context.payload.pull_request.head.ref
 
-            core.info("PR auto-label is enabled for the repo")
-            if (isRepoMultiPrj(pr_head)) {
-                core.info("PLACEHOLDER")
+        // Feature: auto-labeler
+        if (enable_labeler) {
+            core.info("PR auto-label is enabled for the repo ...")
+            // get the head branch of the pull request
+            const pr_head = github.context.payload.pull_request.head.ref
+            var prj_label = getProjectLabel(pr_head)
+
+            if (prj_label) {
+                core.info(`PLACEHOLDER. label: ${prj_label}`)
             }
-            else { core.info("Single-project repo, skipping auto-labeler") }
-        } else { core.info('PR auto-label is disabled for the repo, skipping ...') }
-            
-        core.info("Exiting gracefully ...")
+            else { core.info("Skipping auto-labeler.") }
+        }
+        else { core.info('PR auto-label is disabled for the repo, skipping.') }
+
+        // end of the main block
+        core.info("Exiting gracefully.")
         return
     } 
-    catch (error) {
-        core.setFailed(`Action failed. ${error}`)
-    }
+    catch (error) { core.setFailed(`Action failed. ${error}`) }
 }
 
 
@@ -45,6 +56,7 @@ function logMinimizer(title, text_to_print) {
 }
 
 function logSeparator() {
+    // print out a visual separator into a log
     core.info("=".repeat(80))
 }
 
@@ -52,29 +64,32 @@ function isPrTitleValid(regexes, pr_title) {
     // Validates a PR title against a list of regex patterns
     // Need to match at least one pattern for it to be valid
     // Returns false if not valid, true if valid
-    core.debug("Running 'isPrTitleValid()' ...")
     let pr_title_is_valid = false
     try {
+        // loop over every defined matching pattern
         regexes.forEach(pattern => {
             core.info(`Validating '${pr_title}' against '${pattern}'`)
             const regex = RegExp(pattern)
+
             if (regex.test(pr_title)) {
                 core.info("Match.")
                 pr_title_is_valid = true
-            } else {
-                core.info("Not a match.")
             }
+            else { core.info("Not a match.") }
         })
-
+        
+        // PR need to match at least one pattern for success
         if (pr_title_is_valid) {
             core.info("PR Title matched at least one pattern.")
             core.info("PR Title is valid.")
             return true
         }
+
         core.warning("PR Title did not match against any of the allowed regex patterns.")
         core.warning("Please refer to the PR Title naming policy for your project.")
         return false
-    } catch (error) {
+    } 
+    catch (error) {
         core.error(error)
         core.error("Something went wrong in regex pattern validation!")
         core.error("Contact Fusion DevOps team <fusion_devops@johnsoncontrols365.onmicrosoft.com>")
@@ -82,31 +97,19 @@ function isPrTitleValid(regexes, pr_title) {
     }
 }
 
-function isRepoMultiPrj(head) {
-    // Return true if repo is multi-project, false otherwise.
+function getProjectLabel(head) {
+    // Return project label value if meets conditions; null otherwise
     core.info(`PR head: ${head}`)
-    // branch_name
-    // feature/*
-    // bugfix/*
-    // merge/*
-    // release/*
-    // develop
-    // main
-    
-    // main
-    // main_unstable
-    // prj/develop
-    // prj/feature/*
-    // prj/bugfix/*
-    // prj/merge/*
-    // prj/release/*
-    
-    // branch_name.split()
-    // 1 - if split produces 3 items; then 1st element is project label
+    var items = new Array(head.split('/'))
+    // operates on assumption that:
+    // 1 - if split produces 3 items; then 1st element is a project label
     // 2 - if split produces 2 items and 2nd element is 'develop'; then 1st element is project label
     // everything else is skipped (assumed a single project repo or invalid branch for labeler)
-    
-    return true
+    if (items.length() == 3) { return items[0] }
+    if ((items.length() == 2) && (items[1].toLowerCase() == 'develop')) { return items[0] }
+
+    core.info("branch name did not qualify for a project label extraction.")
+    return null
 }
 
 main()
